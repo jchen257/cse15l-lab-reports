cd847c0c610fce16f8a4adb9055e01b0acdb25c5
jchen257 <97651048+jchen257@users.noreply.github.com>
2022-01-13 19:51:31 -0800

    Delete ScpCommand.java

--- ScpCommand.java
+++
@@ -1,6 +0,0 @@
-class ScpCommand {
-    public static void main(String[] args) {
-      System.out.println("Hello my Operating System is " + System.getProperty("os.name"));
-      System.out.println("My name is " + System.getProperty("user.name"));
-    }
-  }
